c83f7dd5be40c250fa5079ff7e1f0c3a6c074243
yeo4 <yeo4.jo@gmail.com>
2018-03-28 22:01:05 +0300

    Hot update dot_product return double

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -11,19 +11,29 @@ public class Vector {
 	}
 	
 	public Vector add(Vector v) {
-		return null;
+		double _x = v.get().getX().get() + (_head.getX().get());
+		double _y = v.get().getY().get() +(_head.getY().get());
+		double _z = v.get().getZ().get() + (_head.getZ().get());
+		Point3D p = new Point3D(_x,_y,_z);
+		return new Vector(p);
 	}
 	public Vector subtract(Vector v) {
-		return null;
+		double _x = _head.getX().get() - v.get().getX().get();
+		double _y =  _head.getY().get() - v.get().getY().get();
+		double _z = _head.getZ().get() - v.get().getZ().get();
+		Point3D p = new Point3D(_x,_y,_z);
+		return new Vector(p);	
 	}
 	public Vector multiply(Vector v) {
+		
 		return null;
 	}
 	public Vector multiply(double d) {
+		
 		return null;
 	}
-	public Vector dot_product(Vector v) {
-		return null;
+	public double dot_product(Vector v) {
+		return 0;
 	}
 	public double length() {
 		return 0;
