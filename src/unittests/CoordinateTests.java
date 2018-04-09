61fe329a16329d0cad7ba3e8eb935fef38abef3b
yeo4 <yeo4.jo@gmail.com>
2018-04-09 17:54:49 +0300

    Tested.. :)

---
+++ src/unittests/CoordinateTests.java
@@ -0,0 +1,74 @@
+package unittests;
+
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+
+import primitives.*;
+
+class CoordinateTests {
+
+	@Test
+	public void testEquals() {
+		assertFalse("++Different", new Coordinate(1000).equals(new Coordinate(900)));
+		assertFalse("++Close", new Coordinate(1000).equals(new Coordinate(999.999999)));
+		assertTrue("++Very close", new Coordinate(1000).equals(new Coordinate(999.9999999)));
+		assertFalse("++Small Close", new Coordinate(1).equals(new Coordinate(0.999999)));
+		assertTrue("++Small Very close", new Coordinate(1).equals(new Coordinate(0.9999999)));
+	}
+
+	@Test
+	public void testConstructor() {
+		Coordinate zero = new Coordinate(0.0);
+		Coordinate one = new Coordinate(1);
+		Coordinate small4 = new Coordinate(0.0001);
+		Coordinate small6 = new Coordinate(0.000001);
+		Coordinate small7 = new Coordinate(0.0000001);
+		Coordinate small8 = new Coordinate(0.00000001);
+		Coordinate mone = new Coordinate(-1);
+		Coordinate msmall4 = new Coordinate(-0.0001);
+		Coordinate msmall6 = new Coordinate(-0.000001);
+		Coordinate msmall7 = new Coordinate(-0.0000001);
+		Coordinate msmall8 = new Coordinate(-0.00000001);
+		assertNotEquals("One", zero, one);
+		assertNotEquals("0.0001", zero, small4);
+		assertNotEquals("0.000001", zero, small6);
+		assertEquals("0.0000001", zero, small7);
+		assertEquals("0.00000001", zero, small8);
+		assertNotEquals("-One", zero, mone);
+		assertNotEquals("-0.0001", zero, msmall4);
+		assertNotEquals("-0.000001", zero, msmall6);
+		assertEquals("-0.0000001", zero, msmall7);
+		assertEquals("-0.00000001", zero, msmall8);
+	}
+	
+	@Test
+	public void testSubtract() {
+		Coordinate veryBig = new Coordinate(100000000);
+		Coordinate big = new Coordinate(100000);
+		Coordinate med = new Coordinate(1);
+		Coordinate small = new Coordinate(0.0001);
+		Coordinate verySmall = new Coordinate(0.000000001);
+		Coordinate mveryBig = new Coordinate(-100000000);
+		Coordinate mbig = new Coordinate(-100000);
+		Coordinate mmed = new Coordinate(-1);
+		Coordinate msmall = new Coordinate(-0.0001);
+		Coordinate mverySmall = new Coordinate(-0.000000001);
+		Coordinate zero = new Coordinate(0.0);
+		assertNotEquals("Subtract 0", mverySmall, zero);
+		assertNotEquals("Subtract 1", veryBig, veryBig.subtract(big));
+		assertNotEquals("Subtract 1*", mveryBig, big.subtract(veryBig));
+		assertNotEquals("Subtract 1-", veryBig, veryBig.subtract(mbig));
+		assertNotEquals("Subtract -1", mveryBig, mveryBig.subtract(big));
+		assertNotEquals("Subtract -1-", mveryBig, mveryBig.subtract(mbig));
+		assertEquals("Subtract 2", veryBig, veryBig.subtract(med));
+		assertEquals("Subtract 2*", mveryBig, med.subtract(veryBig));
+		assertEquals("Subtract 2-", veryBig, veryBig.subtract(mmed));
+		assertEquals("Subtract -2", mveryBig, mveryBig.subtract(med));
+		assertEquals("Subtract -2-", mveryBig, mveryBig.subtract(mmed));
+		assertNotEquals("Subtract 3", med, med.subtract(small));
+		assertNotEquals("Subtract 3-", med, med.subtract(msmall));
+		assertNotEquals("Subtract -3", mmed, mmed.subtract(small));
+		assertNotEquals("Subtract -3-", mmed, mmed.subtract(msmall));
+		assertEquals("Subtract 4", med, med.subtract(verySmall));
+	}
+}
---
+++ src/unittests/VectorTests.java
@@ -0,0 +1,137 @@
+package unittests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import org.junit.jupiter.api.Test;
+
+import primitives.Point3D;
+import primitives.Vector;
+
+class VectorTests {
+
+	
+	public boolean tmp() {
+		Vector v1 = new Vector(new Point3D(1, 8, 9));
+		Vector v2 = new Vector(v1);
+		Vector v3 = new Vector(new Point3D(99, 23, 7));
+
+		if(v1.equals(v2)) {
+			System.out.println("Test successful: "+v1.toString()+" == " + v2.toString());
+		}else {
+			System.out.println("Test failed: "+v1.toString()+" != " + v2.toString());
+			return false;
+		}
+		System.out.println(v1.add(v2));
+		if(v1.add(v2).equals(new Vector(new Point3D(2,16,18)))) {
+			System.out.println("Test successful: "+v1.add(v2)+" == " + new Vector(new Point3D(2,16,18)));
+		}else {
+			System.out.println("Test failed: "+v1.add(v2)+" != " + new Vector(new Point3D(2,16,18)));
+			return false;
+		}
+		if(v1.add(v3).equals(new Vector(new Point3D(100, 31, 16)))) {
+			System.out.println("Test successful: "+v1.add(v3)+" == " + new Vector(new Point3D(100, 31, 16)));
+		}else {
+			System.out.println("Test failed: "+v1.add(v3)+" != " + new Vector(new Point3D(100, 31, 16)));
+			return false;
+		}
+		Vector tmp = new Vector(new Point3D(-98,-15,2));
+		if(v1.subtract(v3).equals(tmp)) {
+			System.out.println("Test successful: "+v1.subtract(v3)+" == " + tmp);
+		}else {
+			System.out.println("Test failed: "+v1.subtract(v3)+" != " + tmp);
+			return false;
+		}
+		 tmp = new Vector(new Point3D(-151.0,884.0,-769.0));
+		 if(v1.cross_product(v3).equals(tmp)) {
+			System.out.println("Test successful: "+v1.cross_product(v3)+" == " + tmp);
+		}else {
+			System.out.println("Test failed: "+v1.cross_product(v3)+" != " + tmp);
+			return false;
+		}
+		tmp = new Vector(new Point3D(5.0,40.0,45.0));
+		if(v1.multiply(5).equals(tmp)) {
+			System.out.println("Test successful: "+v1.multiply(5)+" == " + tmp);
+		}else {
+			System.out.println("Test failed: "+v1.multiply(5)+" != " + tmp);
+			return false;
+		}
+		
+		
+		System.out.println("/**dot_product**/");
+		
+		if(round(v1.dot_product(v2),3)  == 146) {
+			System.out.println("Test successful: "+v1.dot_product(v2)+" == " + 146);
+		}else {
+			System.out.println("Test failed: "+v1.dot_product(v2)+" != " + 146);
+			return false;
+		}
+		tmp = new Vector(new Point3D(0, -4, 0));
+		if(round(tmp.dot_product(new Vector(new Point3D(0, 4, 0))),3) == -16) {
+			System.out.println("Test successful: "+tmp.dot_product(new Vector(new Point3D(0, 4, 0)))+" == " + -16);
+		}else {
+			System.out.println("Test failed: "+tmp.dot_product(new Vector(new Point3D(0, 4, 0)))+" != " + -16);
+			return false;
+		}
+		
+		tmp = new Vector(new Point3D(0, 1, 0));
+		if(round(tmp.dot_product(new Vector(new Point3D(1, 0, 0))),3) == 0) {
+			System.out.println("Test successful: "+tmp.dot_product(new Vector(new Point3D(1, 0, 0)))+" == " + 0);
+		}else {
+			System.out.println("Test failed: "+tmp.dot_product(new Vector(new Point3D(1, 0, 0)))+" != " + 0);
+			return false;
+		}
+		
+		tmp = new Vector(new Point3D(1, 1, 0));
+		if(round(tmp.dot_product(new Vector(new Point3D(1, 0, 0))),3) == 1) {
+			System.out.println("Test successful: "+tmp.dot_product(new Vector(new Point3D(1, 0, 0)))+" == " + 1);
+		}else {
+			System.out.println("Test failed: "+tmp.dot_product(new Vector(new Point3D(1, 0, 0)))+" != " + 1);
+			return false;
+		}
+
+		
+		tmp = new Vector(new Point3D(-1, 1, 0));
+		if(round(tmp.dot_product(new Vector(new Point3D(1, 0, 0))),3) == -1) {
+			System.out.println("Test successful: "+tmp.dot_product(new Vector(new Point3D(1, 0, 0)))+" == " + -1);
+		}else {
+			System.out.println("Test failed: "+tmp.dot_product(new Vector(new Point3D(1, 0, 0)))+" != " + -1);
+			return false;
+		}
+		return false;
+	}
+	@Test
+	public void testAdd() {
+		Vector v1 = new Vector(new Point3D(1, 8, 9));
+		Vector v2 = new Vector(v1);
+		Vector v3 = new Vector(new Point3D(99, 23, 7));
+		
+		Vector v4 = new Vector(new Point3D(97, 23, 7));
+		Vector v5 = new Vector(new Point3D(43, 20, 2));
+		Vector v6 = new Vector(new Point3D(99, 23, 7));
+
+		
+		assertEquals(v1.add(v2),new Vector(new Point3D(2,16,18)));
+		assertEquals(v1.add(v3),new Vector(new Point3D(100, 31, 16)));
+		
+		assertNotEquals(v1.add(v4),new Vector(new Point3D(100, 31, 5)));
+		assertNotEquals(v1.add(v5),new Vector(new Point3D(76, 20, 31)));
+		assertNotEquals(v1.add(v6),new Vector(new Point3D(456, 1, 0.001)));
+	}
+	@Test
+	public void testSubtract() {
+		fail("Not yet implemented");
+	}
+
+	public double round(double value, int places) {
+	    if (places < 0) throw new IllegalArgumentException();
+
+	    BigDecimal bd = new BigDecimal(value);
+	    bd = bd.setScale(places, RoundingMode.HALF_UP);
+	    return bd.doubleValue();
+	}
+
+
+}
